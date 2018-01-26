2393049aa81400c60d01d0a1bc175d2fdb8d1f0b
Aleksandr Dziuba <a.dziuba@nitralabs.com>
2018-01-26 13:42:18 +0200

    add Butterknife

--- app/src/main/java/ua/kh/zonell/cointest/page/MainActivity.java
+++ app/src/main/java/ua/kh/zonell/cointest/page/MainActivity.java
@@ -1,8 +1,8 @@
 package ua.kh.zonell.cointest.page;
 
+import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ProgressBar;
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -26,10 +28,15 @@ import ua.kh.zonell.cointest.util.Const;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView rvCoin;
+    @BindView(R.id.rvCoin)
+    RecyclerView rvCoin;
+    @BindView(R.id.swipeListCoin)
+    SwipeRefreshLayout swipeListCoin;
+    @BindView(R.id.pbCoin)
+    ProgressBar pbCoin;
+
     private Map<String, Coin> coinMap;
     private Map<String, PriseFull> priseFullMap;
-    private ProgressBar pbCoin;
     private List<Coin> coinList;
     private List<String> priceNameKey;
 
@@ -38,30 +45,27 @@ public class MainActivity extends AppCompatActivity {
 
     private StringBuilder paramsGetPriceCoin;
 
-    private SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         initUI();
     }
 
     private void initUI() {
-        rvCoin = (RecyclerView) findViewById(R.id.rvCoin);
-        pbCoin = (ProgressBar) findViewById(R.id.pbCoin);
         pbCoin.setVisibility(ProgressBar.VISIBLE);
         priceNameKey = new ArrayList<>();
         count = Const.DEFAULT;
         paramsGetPriceCoin = new StringBuilder();
-        refreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeListCoin);
 
         loadListCoin();
         setRefreshListener();
     }
 
-    private void loadCoinPrice(final boolean action){
+    private void loadCoinPrice(final boolean action) {
         coinList = Queries.selectCoin(Const.TEST_LIMIT);
         for (int i = 0; i < coinList.size(); i++) {
             paramsGetPriceCoin.append(coinList.get(i).getName() + Const.SEPARATOR);
@@ -72,11 +76,11 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<PriceResult> call, Response<PriceResult> response) {
                 priseFullMap = response.body().getPriceFull();
 
-                for (String name : priseFullMap.keySet()){
+                for (String name : priseFullMap.keySet()) {
                     priceNameKey.add(name);
                 }
 
-                for (PriseFull priseFull : priseFullMap.values()){
+                for (PriseFull priseFull : priseFullMap.values()) {
                     Queries.insertPriseCoin(priceNameKey.get(count),
                             priseFull.getPriceBTC(), priseFull.getPriceUSD(), priseFull.getPriceEUR(),
                             action);
@@ -97,13 +101,13 @@ public class MainActivity extends AppCompatActivity {
         paramsGetPriceCoin.setLength(Const.DEFAULT);
     }
 
-    private void loadListCoin(){
-        if (Queries.selectCoin().isEmpty()){
+    private void loadListCoin() {
+        if (Queries.selectCoin().isEmpty()) {
             App.getApi(Const.BASE_URL).getListCoin().enqueue(new Callback<CoinResult>() {
                 @Override
                 public void onResponse(Call<CoinResult> call, Response<CoinResult> response) {
                     coinMap = response.body().getCoins();
-                    for (Coin coinMup : coinMap.values()){
+                    for (Coin coinMup : coinMap.values()) {
                         Queries.insertCoin(coinMup);
                     }
                     loadCoinPrice(Const.SELECT);
@@ -118,27 +122,27 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void displayData(List<Coin> coinList){
+    private void displayData(List<Coin> coinList) {
         rvCoin.setLayoutManager(new LinearLayoutManager(this));
         rvCoin.setHasFixedSize(true);
         rvCoin.setAdapter(initCoinListAdapter(coinList));
         pbCoin.setVisibility(ProgressBar.INVISIBLE);
     }
 
-    private CoinListAdapter initCoinListAdapter(List<Coin> coinList){
-        if (coinListAdapter == null){
+    private CoinListAdapter initCoinListAdapter(List<Coin> coinList) {
+        if (coinListAdapter == null) {
             coinListAdapter = new CoinListAdapter(coinList, getApplicationContext());
         }
         return coinListAdapter;
     }
 
-    private void setRefreshListener(){
-        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+    private void setRefreshListener() {
+        swipeListCoin.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 loadCoinPrice(Const.UPDATE);
                 pbCoin.setVisibility(ProgressBar.VISIBLE);
-                refreshLayout.setRefreshing(false);
+                swipeListCoin.setRefreshing(false);
             }
         });
     }
